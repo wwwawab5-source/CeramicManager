cb25332b9b73667beac7e792892237a5bf6881da
wwwawab5-source <www.awab5@gmail.com>
2026-09-11 22:19:25 +0300

    Add files via upload

---
+++ GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using CeramicManager.Data;
+global using CeramicManager.PageModels;
+global using CeramicManager.Pages;
+global using CeramicManager.Services;
+global using CeramicManager.Utilities;
+global using Fonts;
